97bdeb9fd004c1e1ec2c671a7f21a175367e9d34
zehava-drabkin <rdrbig@gmail.com>
2022-03-29 17:19:58 +0300

    feat(rout)change the route

--- router/winning.js
+++ router/winning.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const winningController = require("../controler/winning");
 
-router.get("getWinningByIdOfItem/:id", winningController.getWinningByIdOfItem);
-router.get("getAllUserById/:id", winningController.getAllUserById);
-router.get("getWinning/:id", winningController.getWinning);
+router.get("/getWinningByIdOfItem/:id", winningController.getWinningByIdOfItem);
+router.get("/getAllUserById/:id", winningController.getAllUserById);
+router.get("/getWinning/:id", winningController.getWinning);
 router.post("", winningController.addwinning);
 
 module.exports = router;
